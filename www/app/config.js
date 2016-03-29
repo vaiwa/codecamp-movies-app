4730c2b8b7759f9b91ca86c1494418115e2a33cf
Jan Navrat <jan.navrat@socialbakers.com>
2016-03-29 19:31:35 +0200

    uvozovky

--- www/app/config.js
+++ www/app/config.js
@@ -1,16 +1,16 @@
-app.constant("APP_CONFIG", {
+app.constant('APP_CONFIG', {
 	//Basic config
 	debugMode: true,
-	storagePrefix: "test_",
+	storagePrefix: 'test_',
 
 	// API config
-	apiKey: "4aa883f95999ec813b8bfaf319f3972b",
-	apiUrl: "api.themoviedb.org/3/",
+	apiKey: '4aa883f95999ec813b8bfaf319f3972b',
+	apiUrl: 'api.themoviedb.org/3/',
 	apiEndpoints: {
-		moviesPopular: "movie/popular",
+		moviesPopular: 'movie/popular',
 	},
 
 	getApiUrl: function (endpoint){
-		return "http://" + this.apiUrl + this.apiEndpoints[endpoint] + '?api_key=' + this.apiKey;
+		return 'http://' + this.apiUrl + this.apiEndpoints[endpoint] + '?api_key=' + this.apiKey;
 	},
 });
