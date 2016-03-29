28acaf08b81f9cd10ef1cb30b142d625158af784
Jan Navrat <jan.navrat@socialbakers.com>
2016-03-29 19:20:25 +0200

    apiKey

--- www/app/config.js
+++ www/app/config.js
@@ -4,12 +4,13 @@ app.constant("APP_CONFIG", {
 	storagePrefix: "test_",
 
 	// API config
-	apiUrl: "example.com/api/v1/",
+	apiKey: "4aa883f95999ec813b8bfaf319f3972b",
+	apiUrl: "api.themoviedb.org/3/",
 	apiEndpoints: {
-		exampleApiEndpoint: "endpoint/",
+		exampleApiEndpoint: "movies/popular",
 	},
 
 	getApiUrl: function (endpoint){
-		return "http://" + this.apiUrl + this.apiEndpoints[endpoint];
+		return "http://" + this.apiUrl + this.apiEndpoints[endpoint] + '?api_key=' + this.apiKey;
 	},
 });
